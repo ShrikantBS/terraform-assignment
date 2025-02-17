3fc4038a61fcb4a3c4ed77eec730b8fccb288866
Shrikant Sherkar <Shrikant.Sherkar@accelya.com>
2025-02-17 14:13:36 +0530

    Added description

--- modules/api_gateway/variable.tf
+++ modules/api_gateway/variable.tf
@@ -1,4 +1,21 @@
-variable "stage_name" { default = "prod" }
-variable "rate_limit" { default = 100 }
-variable "cognito_user_pool_arn" {}
-variable "alb_dns_name" {}
+variable "stage_name" { 
+  type    = string
+  default = "prod"
+  description = "API Gateway stage name (e.g., dev, staging, prod)"
+}
+
+variable "rate_limit" { 
+  type    = number
+  default = 100
+  description = "Maximum requests per second allowed by API Gateway"
+}
+
+variable "cognito_user_pool_arn" { 
+  type        = string
+  description = "ARN of the Cognito User Pool for API Gateway authentication"
+}
+
+variable "alb_dns_name" { 
+  type        = string
+  description = "DNS name of the ALB that hosts the backend service"
+}
